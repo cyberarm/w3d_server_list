c14d2f4c5e4ac811a000dd4b2f9530d6f73a6443
cyberarm <matthewlikesrobots@gmail.com>
2022-02-20 08:46:04 -0600

    Handle servers changing their address by checking against their hostname of comparing address:port, added special case for RenCorner Marathon and AOW

--- lib/repair_worker.rb
+++ lib/repair_worker.rb
@@ -32,6 +32,33 @@ class W3DServerList
             end
           end
         end
+
+        # Find duplicates by name i.e. servers that have changed address/port but kept name
+        duplicates = {}
+
+        Server.all.each do |server|
+          key = (server.hostname == "RenCorner AOW" || server.hostname == "RenCorner Marathon") ? "RenCorner Marathon" : server.hostname
+          duplicates[key] ||= []
+
+          # Collect duplicate servers
+          duplicates[key] << server
+        end
+
+        duplicates.each do |key, list|
+          next unless list.size > 1
+
+          target_server = list.pop
+
+          list.each do |server|
+            ActiveRecord::Base.transaction do
+              # Move reports to newest server
+              server.reports.update_all(server_id: target_server.id)
+
+              # Remove duplicate servers
+              server.destroy
+            end
+          end
+        end
       end
     end
   end
